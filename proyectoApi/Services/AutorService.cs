e32d06861c4f5b82771d2cecd330b92991aaf49d
Leonel Herrera <leonel48473@hotmail.es>
2022-09-09 09:56:23 -0400

    Actualizacion servicio autor

--- proyectoApi/Services/AutorService.cs
+++ proyectoApi/Services/AutorService.cs
@@ -9,12 +9,13 @@ namespace proyectoApi.Services
     public class AutorService : IAutorService
     {
         // Dependencias
-        private readonly db_libreriaContext _context;
+        //private readonly db_libreriaContext _context;
+        private readonly PruebaContext _context;
         //Para obtener el connectionString
         private readonly IConfiguration _configuration;
 
         // Pasamos las dependencias en el constructor
-        public AutorService(db_libreriaContext context, IConfiguration configuration)
+        public AutorService(/*db_libreriaContext context*/ IConfiguration configuration, PruebaContext context)
         {
 
             _context = context;
@@ -23,44 +24,49 @@ namespace proyectoApi.Services
 
         public Autor BuscarAutor(BuscarAutorVM vm)
         {
-            try
+            /*
+                Con stored procedure
+
+            using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("ConexionLibreria")))
             {
-                // Ejecutando stored procedure
-                using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("ConexionLibreria")))
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "Search_Autor";
+                cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = vm.nombre;
+                cmd.Parameters.Add("@Apellidos", SqlDbType.VarChar).Value = vm.apellido;
+                cmd.Connection = conn;
+                conn.Open();
+                SqlDataReader resultado = cmd.ExecuteReader();
+                Autor autor = new Autor();
+                while (resultado.Read())
                 {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "Search_Autor";
-                    cmd.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = vm.nombre;
-                    cmd.Parameters.Add("@Apellidos", SqlDbType.VarChar).Value = vm.apellido;
-                    cmd.Connection = conn;
-                    conn.Open();
-                    SqlDataReader resultado = cmd.ExecuteReader();
-                    Autor autor = new Autor();
-                    while (resultado.Read())
-                    {
-                        autor.id_autor = resultado[0].ToString();
-                        autor.apellido = resultado[1].ToString();
-                        autor.nombre = resultado[2].ToString();
-                        autor.telefono = resultado[3].ToString();
-                        autor.direccion = resultado[4].ToString();
-                        autor.ciudad = resultado[5].ToString();
-                        autor.estado = resultado[6].ToString();
-                        autor.pais = resultado[7].ToString();
-                        autor.cod_postal = (int)resultado[8];
-                        autor.Genero = resultado[9].ToString();
+                    autor.id_autor = resultado[0].ToString();
+                    autor.apellido = resultado[1].ToString();
+                    autor.nombre = resultado[2].ToString();
+                    autor.telefono = resultado[3].ToString();
+                    autor.direccion = resultado[4].ToString();
+                    autor.ciudad = resultado[5].ToString();
+                    autor.estado = resultado[6].ToString();
+                    autor.pais = resultado[7].ToString();
+                    autor.cod_postal = (int)resultado[8];
+                    autor.Genero = resultado[9].ToString();
 
-                    }
-                    conn.Close();
-                    return autor;
-                };
-
-            }
-            catch
+                }
+                conn.Close()
+            };
+            */
+            var autorResultado = _context.Personas.Where(x => x.Nombre == vm.nombre && x.Apellido == vm.apellido).ToList();
+            Autor autor = new Autor();
+            foreach(var item in autorResultado)
             {
-                return null;
+                autor.nombre = item.Nombre;
+                autor.apellido = item.Apellido;
+                autor.direccion = item.Direccion;
+                autor.Genero = item.Sexo;
             }
 
+            return autor;
+            
         }
 
 
--- proyectoApi/Services/SolicitudService.cs
+++ proyectoApi/Services/SolicitudService.cs
@@ -16,28 +16,19 @@ namespace proyectoApi.Services
             _context = context;
         }
 
-
         public async Task<bool> UpdateState(cambiarEstadoVM vm)
         {
-            try
-            {
                 // Buscar solicitud que tenga ese id
                 var soli = await _context.Solicituds.FindAsync(vm.id);
+
+                if (soli is null) return false;
 
                 // Cambio estado por el nuevo
                 soli.EstadoId = vm.estado;
 
-
                 // Guardar los cambios
                 await _context.SaveChangesAsync();
                 return true;
-
-            }
-            catch
-            {
-                return false;
-            }
-
         }
     }
 }
